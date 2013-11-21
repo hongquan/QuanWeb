89e2508b406270c5dab8e4a11c73a899dfc56bf6
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2013-11-22 00:38:40 +0800

    Change space and marks.

--- static/js/tracking.js
+++ static/js/tracking.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   //Configure the jQuery cookie plugin to use JSON.
   $.cookie.json = true;
@@ -8,37 +8,37 @@ $(document).ready(function(){
   sessionExpireTime.setMinutes(sessionExpireTime.getMinutes()+30);
 
   //Check if we have a session cookie:
-  var session_cookie = $.cookie("session_cookie");
+  var session_cookie = $.cookie('session_cookie');
 
   //If it is undefined, set a new one.
-  if(session_cookie == undefined){
-    $.cookie("session_cookie", {
+  if(session_cookie == undefined) {
+    $.cookie('session_cookie', {
       id: Math.uuid()
     }, {
       expires: sessionExpireTime,
-      path: "/" //Makes this cookie readable from all pages
+      path: '/' //Makes this cookie readable from all pages
     });
   }
   //If it does exist, delete it and set a new one with new expiration time
-  else{
-    $.removeCookie("session_cookie", {
-      path: "/"
+  else {
+    $.removeCookie('session_cookie', {
+      path: '/'
     });
-    $.cookie("session_cookie", session_cookie, {
+    $.cookie('session_cookie', session_cookie, {
       expires: sessionExpireTime,
-      path: "/"
+      path: '/'
     });
   }
 
-  var permanent_cookie = $.cookie("permanent_cookie");
+  var permanent_cookie = $.cookie('permanent_cookie');
 
   //If it is undefined, set a new one.
-  if(permanent_cookie == undefined){
-    $.cookie("permanent_cookie", {
+  if(permanent_cookie == undefined) {
+    $.cookie('permanent_cookie', {
       id: Math.uuid()
     }, {
       expires: 3650, //10 year expiration date
-      path: "/" //Makes this cookie readable from all pages
+      path: '/' //Makes this cookie readable from all pages
     });
   }
 
@@ -48,30 +48,30 @@ $(document).ready(function(){
   var parser = new UAParser();
 
   var eventProperties = {
-    session_id: $.cookie("session_cookie")["id"],
+    session_id: $.cookie('session_cookie')['id'],
     url: {
-      source: parsedUrl.attr("source"),
-      protocol: parsedUrl.attr("protocol"),
-      domain: parsedUrl.attr("host"),
-      port: parsedUrl.attr("port"),
-      path: parsedUrl.attr("path"),
+      source: parsedUrl.attr('source'),
+      protocol: parsedUrl.attr('protocol'),
+      domain: parsedUrl.attr('host'),
+      port: parsedUrl.attr('port'),
+      path: parsedUrl.attr('path'),
       query_params: parsedUrl.param(),
-      anchor: parsedUrl.attr("anchor")
+      anchor: parsedUrl.attr('anchor')
     },
     user_agent: {
       browser: parser.getBrowser(),
       engine: parser.getEngine(),
       os: parser.getOS()
     },
-    permanent_tracker: $.cookie("permanent_cookie")["id"]
+    permanent_tracker: $.cookie('permanent_cookie')['id']
   };
 
   /*
   //If you know that the user is currently logged in, add information about the user.
-  eventProperties["user"] = {
-    id: "",
-    signupDate: ""
-    etc: ".."
+  eventProperties['user'] = {
+    id: '',
+    signupDate: ''
+    etc: '..'
   };
   */
 
@@ -79,22 +79,22 @@ $(document).ready(function(){
   var referrer = document.referrer;
   referrerObject = null;
 
-  if(referrer != undefined){
+  if(referrer != undefined) {
     parsedReferrer = $.url(referrer);
 
     referrerObject = {
-      source: parsedReferrer.attr("source"),
-      protocol: parsedReferrer.attr("protocol"),
-      domain: parsedReferrer.attr("host"),
-      port: parsedReferrer.attr("port"),
-      path: parsedReferrer.attr("path"),
+      source: parsedReferrer.attr('source'),
+      protocol: parsedReferrer.attr('protocol'),
+      domain: parsedReferrer.attr('host'),
+      port: parsedReferrer.attr('port'),
+      path: parsedReferrer.attr('path'),
       query_params: parsedReferrer.param(),
-      anchor: parsedReferrer.attr("anchor")
+      anchor: parsedReferrer.attr('anchor')
     }
   }
 
-  eventProperties["referrer"] = referrerObject;
+  eventProperties['referrer'] = referrerObject;
 
-  Keen.addEvent("pageviews", eventProperties)
+  Keen.addEvent('pageviews', eventProperties)
 
 });
