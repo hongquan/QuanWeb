1b128dbb8527379c8429408a41d0ec8c2aedc008
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2013-11-29 23:09:20 +0800

    Exclude tracking script when runningmlocally.

--- quanweb/views/context_processors.py
+++ quanweb/views/context_processors.py
@@ -1,6 +1,14 @@
+from flask import request
 from ..common import app
 from .. import config
 
 @app.context_processor
 def inject_config():
     return {'config': config}
+
+@app.context_processor
+def is_running_locally():
+    hostname, port = request.host.split(':')
+    if port == '5000':
+        return {'running_locally': True}
+    return {}
