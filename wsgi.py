42812f89f2a1143418fd6c0b896fa6ddc77a9adf
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2014-12-11 11:51:23 +0800

    Re-enable logentries.

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,21 @@
 import logging
-#from logentries import LogentriesHandler
+from logentries import LogentriesHandler
 from waitress import serve
 import appenlight_client.ext.flask as appenlight
 
 from quanweb import app
-from quanweb.config import APPENLIGHT_PRVKEY
+from quanweb.config import APPENLIGHT_PRVKEY, LOGENTRIES_TOKEN
 
 ADDRESS = '127.0.0.1'
 PORT = 2750
 
-#handler = LogentriesHandler(LOGENTRIES_TOKEN)
-#app.logger.addHandler(handler)
-#logging.getLogger('sqlalchemy').addHandler(handler)
-app = appenlight.add_appenlight(app, {'appenlight.api_key': APPENLIGHT_PRVKEY})
+if LOGENTRIES_TOKEN:
+    handler = LogentriesHandler(LOGENTRIES_TOKEN)
+    app.logger.addHandler(handler)
+    logging.getLogger('sqlalchemy').addHandler(handler)
+
+if APPENLIGHT_PRVKEY:
+    app = appenlight.add_appenlight(app, {'appenlight.api_key': APPENLIGHT_PRVKEY})
 
 if __name__ == '__main__':
     serve(app, host=ADDRESS, port=PORT)
