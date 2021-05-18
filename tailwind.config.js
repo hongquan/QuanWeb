d7cacb87c40e0786b8897fd845b3232e3482a680
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2021-05-18 15:01:11 +0700

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
