ccf5cc3e9fd5ab6fc2d31dc9b8d1b7a984cda3de
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2023-07-02 23:32:08 +0700

    Don't use hex for secret. Just use ASCII secret.

--- src/conf.rs
+++ src/conf.rs
@@ -1,17 +1,21 @@
-
-use hex;
-use rand::Rng;
+use libpassgen::{generate_password, Pool};
 use miette::{miette, Report};
 
 use config::{Config, ConfigError, File};
 
-pub const KEY_SECRET: &'static str = "secret_hex";
+pub const KEY_SECRET: &'static str = "secret_key";
 pub const KEY_PORT: &str = "port";
 pub const DEFAULT_PORT: u16 = 3721;
+pub const ALPHANUMERIC: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+pub fn gen_fallback_secret() -> String {
+    let pool: Pool = ALPHANUMERIC.parse().unwrap_or_default();
+    // 64 is the secret bytes count required by axum-sessions
+    generate_password(&pool, 64).into()
+}
 
 pub fn get_config() -> Result<Config, ConfigError> {
-    let fallback_secret = rand::thread_rng().gen::<[u8; 64]>();
-    let fallback_secret = hex::encode(fallback_secret);
+    let fallback_secret = gen_fallback_secret();
     Config::builder()
         .set_default(KEY_SECRET, fallback_secret)?
         .set_default(KEY_PORT, DEFAULT_PORT)?
@@ -21,13 +25,17 @@ pub fn get_config() -> Result<Config, ConfigError> {
 }
 
 pub fn get_secret_bytes(config: &Config) -> Result<Vec<u8>, Report> {
-    let secret_hex = config.get_string(KEY_SECRET).map_err(|e| miette!("Failed to get secret hex: {e}"))?;
-    tracing::info!("Secret hex: {}", secret_hex);
-    let secret_bytes = hex::decode(secret_hex).map_err(|e| miette!("Invalid secret hex string {e}"))?;
-    Ok(secret_bytes)
+    let secret_str = config
+        .get_string(KEY_SECRET)
+        .map_err(|e| miette!("Failed to get secret key: {e}"))?;
+    tracing::info!("Secret key: {}", secret_str);
+    Ok(secret_str.as_bytes().into())
 }
 
 pub fn get_listening_port(config: &Config) -> u16 {
-    let port = config.get_int(KEY_PORT).map_err(|e| miette!("Failed to get port: {e}")).map(|p| p as u16);
+    let port = config
+        .get_int(KEY_PORT)
+        .map_err(|e| miette!("Failed to get port: {e}"))
+        .map(|p| p as u16);
     port.unwrap_or(DEFAULT_PORT)
 }
