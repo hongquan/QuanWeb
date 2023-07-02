de024cced7a9d839dd256f93427393e05b7b2b86
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2023-07-02 15:16:16 +0700

    Shorter declaration of routes

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -1,22 +1,26 @@
-use axum::routing::{Router, get, post, delete, patch};
+use axum::routing::{get, post, Router};
 
-use crate::types::SharedState;
-use super::views;
 use super::auth;
+use super::views;
+use crate::types::SharedState;
 
 pub fn get_router() -> Router<SharedState> {
+    let single_post_router = get(views::get_post)
+        .patch(views::update_post_partial)
+        .delete(views::delete_post);
+    let single_category_router = get(views::get_category)
+        .patch(views::update_category_partial)
+        .delete(views::delete_category);
+
     Router::new()
         .route("/", get(views::root))
         .route("/login", post(auth::login))
         .route("/users/me", get(views::show_me))
-        .route("/posts/", get(views::list_posts))
-        .route("/posts/:post_id", get(views::get_post))
-        .route("/posts/:post_id", delete(views::delete_post))
-        .route("/posts/:post_id", patch(views::update_post_partial))
-        .route("/posts/", post(views::create_post))
-        .route("/categories/", get(views::list_categories))
-        .route("/categories/:category_id", get(views::get_category))
-        .route("/categories/:category_id", delete(views::delete_category))
-        .route("/categories/:category_id", patch(views::update_category_partial))
-        .route("/categories/", post(views::create_category))
+        .route("/posts/", get(views::list_posts).post(views::create_post))
+        .route("/posts/:post_id", single_post_router)
+        .route(
+            "/categories/",
+            get(views::list_categories).post(views::create_category),
+        )
+        .route("/categories/:category_id", single_category_router)
 }
