5e7941940011ff447e46e107898a7c2035c04352
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2023-07-04 13:17:03 +0700

    Successfully rendering with MiniJinja

--- src/views/front.rs
+++ src/views/front.rs
@@ -1,14 +1,18 @@
 use axum::extract::State;
 use axum::response::{Html, Result as AxumResult};
 use axum_template::TemplateEngine;
+use minijinja::context;
 
 use crate::types::AppState;
 use crate::retrievers;
 use crate::errors::PageError;
+use crate::models::BlogPost;
 
 pub async fn home(State(state): State<AppState>) -> AxumResult<Html<String>> {
     let AppState { db, template_engine } = state;
     let result = retrievers::get_blogposts(Some(0), Some(10), &db).await.map_err(PageError::EdgeDBQueryError)?;
-    let html = template_engine.render("home.jinja", &result)?;
+    let posts: Vec<BlogPost> = result.into_iter().collect();
+    let context = context!(posts => posts);
+    let html = template_engine.render("home.jinja", context)?;
     Ok(Html(html))
 }
