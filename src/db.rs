f92a0201e27e2bb3725c7499655bf53b98071f50
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2023-07-10 09:47:26 +0700

    Change connection config to adapt to live system

--- src/db.rs
+++ src/db.rs
@@ -1,22 +1,26 @@
 use async_fred_session::RedisSessionStore;
-use edgedb_errors::ErrorKind;
-use fred::{pool::RedisPool, types::RedisConfig, error::RedisError};
 use config::Config;
 use edgedb_errors::kinds::ConfigurationError;
+use edgedb_errors::ErrorKind;
+use edgedb_tokio::TlsSecurity;
+use fred::{error::RedisError, pool::RedisPool, types::RedisConfig};
 
-use crate::consts::DB_NAME;
 use crate::conf::KEY_EDGEDB_INSTANCE;
+use crate::consts::DB_NAME;
 
-pub async fn get_edgedb_client(app_config: &Config) -> Result<edgedb_tokio::Client, edgedb_tokio::Error> {
+pub async fn get_edgedb_client(
+    app_config: &Config,
+) -> Result<edgedb_tokio::Client, edgedb_tokio::Error> {
     let mut builder = edgedb_tokio::Builder::new();
-    builder.database(DB_NAME)?;
+    // Our EdgeDB is only available on localhost, so no need for TLS.
+    builder.tls_security(TlsSecurity::Insecure);
     let instance_name = app_config.get_string(KEY_EDGEDB_INSTANCE).map_err(|e| {
         tracing::error!("Missing EdgeDB instance name in config");
         ConfigurationError::with_message(e.to_string())
     })?;
     tracing::info!("To connect to EdgeDB instance {}", instance_name);
     builder.instance(&instance_name)?;
-    let config = builder.build_env().await?;
+    let config = builder.build_env().await?.with_database(DB_NAME)?;
     Ok(edgedb_tokio::Client::new(&config))
 }
 
