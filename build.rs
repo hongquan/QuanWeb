17096198681f973c886ce16c458f4b600ce2b4e4
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2023-07-24 13:06:23 +0700

    Auto build frontend when we build the Rust backend

--- build.rs
+++ build.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::process::Command;
+
 fn main() {
+    // Build frontend also if we are in release mode
+    if env::var("PROFILE") == Ok("release".into()) {
+        eprintln!("To build frontend in admin...");
+        Command::new("yarn")
+            .args(&["--cwd", "admin", "build"])
+            .status()
+            .expect("Failed to build frontend");
+    }
     crate_git_revision::init();
 }
