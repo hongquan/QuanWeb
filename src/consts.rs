3494a6aa202869a324d78c1eb99389fb46079ff4
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2024-03-31 13:17:08 +0700

    Restore SYNTECT_CLASS_STYLE

--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,11 @@
+use syntect::html::ClassStyle;
+
 pub const DB_NAME: &str = "quanweb";
 pub const DEFAULT_PAGE_SIZE: u8 = 10;
 pub const STATIC_URL: &str = "/static";
 pub const UNCATEGORIZED_URL: &str = "/category/_uncategorized/";
+#[allow(dead_code)]
+pub const SYNTECT_CLASS_STYLE: ClassStyle = ClassStyle::SpacedPrefixed { prefix: "st-" };
 pub const KEY_LANG: &str = "lang";
 pub const DEFAULT_LANG: &str = "en";
 pub const ALPINE_HIGHLIGHTING_APP: &str = "need_highlight";
