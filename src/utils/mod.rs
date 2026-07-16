db6008931ca2b85d7dfa41a89a2961df75f1da50
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2026-07-16 11:38:26 +0700

    Fix bug with searching: Return all posts intead of filter.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -6,5 +6,24 @@ pub mod urls;
 pub fn split_search_query(query: Option<&str>) -> Option<Vec<&str>> {
     let tokens: Option<Vec<&str>> =
         query.map(|s| s.split_whitespace().filter(|&s| !s.is_empty()).collect());
-    tokens.filter(|v| v.is_empty())
+    tokens.filter(|v| !v.is_empty())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_split_search_query_single_word() {
+        let query = Some("Linux");
+        let expected: Option<Vec<&str>> = Some(vec!["Linux"]);
+        assert_eq!(split_search_query(query), expected);
+    }
+
+    #[test]
+    fn test_split_search_query_multiple_words() {
+        let query = Some("Linux kernel");
+        let expected: Option<Vec<&str>> = Some(vec!["Linux", "kernel"]);
+        assert_eq!(split_search_query(query), expected);
+    }
 }
